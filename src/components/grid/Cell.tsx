8927cd64f1abe859f1dcab8bf17764c3e346031b
zaram <zaramann312@gmail.com>
2022-02-13 23:51:26 -0800

    add bg

--- src/components/grid/Cell.tsx
+++ src/components/grid/Cell.tsx
@@ -26,10 +26,10 @@ export const Cell = ({
   const classes = classnames(
     'w-14 h-14 border-solid border-2 flex items-center justify-center mx-0.5 text-4xl font-bold rounded dark:text-white',
     {
-      'bg-white dark:bg-slate-900 border-slate-200 dark:border-slate-600':
+      'bg-white dark:bg-gray-900 border-gray-200 dark:border-gray-600':
         !status,
-      'border-black dark:border-slate-100': value && !status,
-      'absent shadowed bg-slate-400 dark:bg-slate-700 text-white border-slate-400 dark:border-slate-700':
+      'border-black dark:border-gray-100': value && !status,
+      'absent shadowed bg-gray-400 dark:bg-gray-700 text-white border-gray-400 dark:border-gray-700':
         status === 'absent',
       'correct shadowed bg-orange-500 text-white border-orange-500':
         status === 'correct' && isHighContrast,
