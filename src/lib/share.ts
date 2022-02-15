fd05e9bbc57bb917029e44308051f19b5cfa2efc
zaram <zaramann312@gmail.com>
2022-02-14 22:30:35 -0800

    add link to share

--- src/lib/share.ts
+++ src/lib/share.ts
@@ -11,7 +11,7 @@ export const shareStatus = (
   navigator.clipboard.writeText(
     `${GAME_TITLE} ${solutionIndex} ${lost ? 'X' : guesses.length}/6${
       isHardMode ? '*' : ''
-    }\n\n` + generateEmojiGrid(guesses)
+    }\n\n` + generateEmojiGrid(guesses) + `https://aot-wordle.vercel.app/`
   )
 }
 
